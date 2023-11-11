3bcb277f8bf7ebc31c4c0f86faf5808a2c1fd773
vtg04 <122604450+vtg04@users.noreply.github.com>
2023-11-11 13:56:13 +0530

    Update and rename adj_res4.py to adjust_results4_isadog.py

--- adjust_results4_isadog.py
+++ adjust_results4_isadog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/adj_res4.py
+# */AIPND-revision/intropyproject-classify-pet-images/adjust_results4_isadog.py
 #                                                                             
 # PROGRAMMER:Vinay
 # DATE CREATED: 10.11.2023                                 
