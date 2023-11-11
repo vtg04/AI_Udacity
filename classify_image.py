d3ea35cb0ebc17cbe4fc7f890ab6819004f4e6ce
vtg04 <122604450+vtg04@users.noreply.github.com>
2023-11-11 14:05:55 +0530

    Update and rename classify_img.py to classify_image.py

--- classify_image.py
+++ classify_image.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
 #                                                                             
-# PROGRAMMER: Christian Edelmayer
-# DATE CREATED: 10.01.2022                                
+# PROGRAMMER:Vinay
+# DATE CREATED: 10.11.2023                                
 # REVISED DATE: 
 # PURPOSE: Create a function classify_images that uses the classifier function 
 #          to create the classifier labels and then compares the classifier 
